7c82fc5bbf6a430c7d27d30d5b9671790fd90575
mtumilowicz <michal.tumilowicz01@gmail.com>
2022-10-23 22:59:34 +0200

    creating generator

--- src/test/scala/generators/AccountGenerators.scala
+++ src/test/scala/generators/AccountGenerators.scala
@@ -1,11 +1,18 @@
 package generators
 
-import app.Account
-import app.contributors.{ContributorService, Repository}
+import app.{Account, AccountId, AccountStatus}
 import zio.test.Gen
 import zio.test.magnolia.DeriveGen
 import RefinedTypesGenerators._
+import eu.timepit.refined.types.string.NonEmptyString
 
 object AccountGenerators {
   val genAccount: Gen[Any, Account] = DeriveGen[Account]
+
+  val genAccount2: Gen[Any, Account] =
+    (Gen.uuid <*>
+      Gen.fromIterable(AccountStatus.values) <*>
+      Gen.string1(Gen.char)
+      ).map { case (uuid, status, str) => Account(AccountId(uuid), status, NonEmptyString.unsafeFrom(str))
+    }
 }
